b4be55a9d4160de8425108a7c8ed27413fea1381
mavo97 <mavodeveloper@gmail.com>
2021-01-26 13:14:37 -0600

    Segmento controller

--- controllers/segmento-controller.js
+++ controllers/segmento-controller.js
@@ -56,7 +56,7 @@ async function saveSegmento(req, res) {
                                 @fechaActualizacion, @fechaCreacion);`);
     }
 
-    sql.close();
+    // sql.close();
     console.log("Todo bien");
     return res.status(200).send({ segmento: "Segmento guardado!" });
   } catch (error) {
